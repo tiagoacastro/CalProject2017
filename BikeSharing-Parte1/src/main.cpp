a3e4a161c0325b7f0ce4ae46d9100fef80fd6bee
Tiago Castro <tiagoaraujocastro@gmail.com>
2018-04-08 18:14:13 +0100

    comented main

--- BikeSharing-Parte1/src/main.cpp
+++ BikeSharing-Parte1/src/main.cpp
@@ -76,9 +76,9 @@ int main()
     //vector <Node> nodes = p.readNodes("C:\\Users\\jonas\\Desktop\\CAL-PROJ\\BikeSharing-Parte1\\Nodes.txt", sharingSpots);
     //vector <Street> streets = p.readStreets("C:\\Users\\jonas\\Desktop\\CAL-PROJ\\BikeSharing-Parte1\\Streets.txt");
     //p.readRelations("C:\\Users\\jonas\\Desktop\\CAL-PROJ\\BikeSharing-Parte1\\Relations.txt", streets,nodes);
-    vector <Node> nodes = p.readNodes("C:\\Users\\tiago\\Desktop\\Faculdade\\CAL\\CalProject2017\\BikeSharing-Parte1\\Nodes.txt", sharingSpots);
-    vector <Street> streets = p.readStreets("C:\\Users\\tiago\\Desktop\\Faculdade\\CAL\\CalProject2017\\BikeSharing-Parte1\\Streets.txt");
-    p.readRelations("C:\\Users\\tiago\\Desktop\\Faculdade\\CAL\\CalProject2017\\BikeSharing-Parte1\\Relations.txt", streets,nodes);
+    //vector <Node> nodes = p.readNodes("C:\\Users\\tiago\\Desktop\\Faculdade\\CAL\\CalProject2017\\BikeSharing-Parte1\\Nodes.txt", sharingSpots);
+    //vector <Street> streets = p.readStreets("C:\\Users\\tiago\\Desktop\\Faculdade\\CAL\\CalProject2017\\BikeSharing-Parte1\\Streets.txt");
+    //p.readRelations("C:\\Users\\tiago\\Desktop\\Faculdade\\CAL\\CalProject2017\\BikeSharing-Parte1\\Relations.txt", streets,nodes);
     //vector <Node> nodes = p.readNodes("Nodes.txt", sharingSpots);
     //vector <Street> streets = p.readStreets("Streets.txt");
    // p.readRelations("Relations.txt",streets,nodes);
@@ -89,9 +89,9 @@ int main()
 
    // cout << nodes[0].getLongitude() << "   " << nodes[0].getLatitude() << endl;
 
-    BikeCompany company(nodes, streets, sharingSpots, user); //Not working cause every street has idNodeInicial = 4 which doesn't exist.
+    //BikeCompany company(nodes, streets, sharingSpots, user); //Not working cause every street has idNodeInicial = 4 which doesn't exist.
 
-    company.printGraph();
+    //company.printGraph();
     //company.getNearestSharingSpot(nodes[0]);
 
 
